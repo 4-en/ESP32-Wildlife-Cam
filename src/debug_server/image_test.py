f26c9acd7742730ebb387e40921f932c71691feb
4-en <ben.schumacher0@gmail.com>
2024-05-14 23:01:39 +0200

    add test server for cam module

---
+++ src/debug_server/image_test.py
@@ -0,0 +1,34 @@
+# test server for esp32 cam
+# receive image from esp32 cam and show it
+
+from fastapi import FastAPI, File, UploadFile
+import cv2
+
+app = FastAPI()
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+    with open(file.filename, "wb") as f:
+        f.write(file.file.read())
+    return {"filename": file.filename}
+
+@app.post("/showimage/")
+async def show_image(file: UploadFile = File(...)):
+    with open(file.filename, "wb") as f:
+        f.write(file.file.read())
+    img = cv2.imread(file.filename)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return {"filename": file.filename}
+
+@app.post("/printmessage/")
+async def print_message(message: str):
+    print(message)
+    return {"message": message}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
