548314b6f528bf806ef3d50111fe1cf19a653659
4-en <ben.schumacher0@gmail.com>
2024-05-15 23:21:16 +0200

    testing tf lite

--- src/wildlife_camera/lib/Test/Test.cpp
+++ src/wildlife_camera/lib/Test/Test.cpp
@@ -1,5 +1,10 @@
 #include "Test.h"
 #include <Arduino.h>
+// tensorflow lite
+//#include <TensorFlowLite_ESP32.h>
+#include <tensorflow/lite/micro/all_ops_resolver.h>
+#include <tensorflow/lite/micro/micro_error_reporter.h>
+#include <tensorflow/lite/micro/micro_interpreter.h>
 
 int test_square(int n) {
     Serial.println("Test square function");
