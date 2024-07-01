4fe1574a3efad4973db08cc47bbb724a502fdcde
4-en <ben.schumacher0@gmail.com>
2024-07-01 14:41:34 +0200

    test serial connection

---
+++ src/feature_tests/serial_test/serial_test.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+}
+
+int count = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print("Hello world: ");
+  Serial.println(count);
+  delay(1000);
+  count++;
+}
